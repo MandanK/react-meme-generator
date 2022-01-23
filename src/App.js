b4d4422035ef702f12dc5b7e511776fdc7c849d9
Mandan Kazzazi <92727403+MandanK@users.noreply.github.com>
2022-01-23 18:22:54 +0100

    updating user interface design

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 // // import { css } from '@emotion/react';
 import React, { useEffect, useState } from 'react';
 
-// A function for downloading; reference: https://stackoverflow.com/questions/49668363/html5-download-attribute-not-working-when-download-external-pdf-file-on-chrome
+// * A function to download the URL modified by user;
+// * Thanks to Karl, reference: https://stackoverflow.com/questions/49668363/html5-download-attribute-not-working-when-download-external-pdf-file-on-chrome
+
 function forceDownload(blob, filename) {
-  // Create an invisible anchor element
+  // * Create an invisible anchor element
   const anchor = document.createElement('a');
   anchor.style.display = 'none';
   anchor.href = window.URL.createObjectURL(blob);
   anchor.setAttribute('download', filename);
   document.body.appendChild(anchor);
 
-  // Trigger the download by simulating click
+  // * Trigger the download by simulating click
   anchor.click();
 
-  // Clean up
+  // * Clean up
   window.URL.revokeObjectURL(anchor.href);
   document.body.removeChild(anchor);
 }
 function downloadResource(URL, filename) {
-  // If we dont know what is the filename, let's produce just a number
+  // * If we dont know what is the filename, let's produce just a number
   if (!filename) filename = Math.random();
   fetch(URL, {
     headers: new Headers({
@@ -32,21 +34,26 @@ function downloadResource(URL, filename) {
     .then((blob) => forceDownload(blob, filename))
     .catch((e) => console.error(e));
 }
-
+// * Function to define the variables
+//* and also to create the useStates
+// * in order to make the text boxes
+// * the template names in selector element
+// * and also the url of the templates
+// * that we have used on the function above
 function App() {
-  const [template, setTemplate] = useState([]);
+  const [memeTemplate, setMemeTemplate] = useState([]);
   const [topText, setTopText] = useState('To be or not to be...');
   const [bottomText, setBottomText] = useState('This is the question!');
-  const [templateName, setTemplateName] = useState('');
+  const [memeTemplateName, setMemeTemplateName] = useState('');
   const [url, setUrl] = useState('https://api.memegen.link/images/ds.png');
 
-  // Let's fetch the templates
+  // * Let's fetch the templates, the code was found in internet
   useEffect(() => {
     const templateToFetch = async () => {
       try {
         const response = await fetch('https://api.memegen.link/templates/');
         const json = await response.json();
-        setTemplate(json);
+        setMemeTemplate(json);
       } catch (e) {
         console.error(e);
       }
@@ -59,9 +66,11 @@ function App() {
   return (
     <main>
       <div className="App">
+        {/* Left and right ids are used in CSS only.*/}
         <div id="left">
-          <h1 className="App-header">Random meme Generator</h1>
+          <h1 className="App-header">Meme Generator</h1>
           <form>
+            {/* Labels to make text boxes}*/}
             <label htmlFor="top_text"> Top text</label>
             <input
               name="top_text"
@@ -82,22 +91,25 @@ function App() {
             <input
               name="template_name"
               id="template_name"
-              value={templateName}
-              onChange={(e) => setTemplateName(e.target.value)}
+              value={memeTemplateName}
+              onChange={(e) => setMemeTemplateName(e.target.value)}
             />
             <br />
           </form>
-          <div className="inputArea">
+          {/* To create a selector elements for memes templates*/}
+          <div className="userInputSection">
             <label htmlFor="image">Meme template: </label>
+            {/* Creat a selector to list the memes templates */}
             <select
               id="image"
               placeholder="buzz"
-              value={templateName}
+              value={memeTemplateName}
               onChange={(event) => {
-                setTemplateName(event.currentTarget.value);
+                setMemeTemplateName(event.currentTarget.value);
               }}
             >
-              {template.map((item) => (
+              {/* Create a map to render an array of templates for the selector*/}
+              {memeTemplate.map((item) => (
                 <option value={item.id} key={item.id}>
                   {item.name}
                 </option>
@@ -107,20 +119,23 @@ function App() {
           <br />
           <div>
             <button
+              // * Create a button to send the modified URL based on the user template selection and text entry to API
               class="previewButton"
               onClick={() => {
+                // * As you can see, users by choosing the template, typing the text, modify the URL and this new version will be send to API
                 setUrl(
-                  `https://api.memegen.link/images/${templateName}/${topText}/${bottomText}.jpg`,
+                  `https://api.memegen.link/images/${memeTemplateName}/${topText}/${bottomText}.jpg`,
                 );
               }}
             >
-              Preview meme
+              Preview
             </button>
             <button
               class="downloadButton"
               onClick={() => {
+                // * The download function on the top allows users to download the selected memes
                 downloadResource(
-                  `https://api.memegen.link/images/${templateName}/${topText}/${bottomText}.jpg`,
+                  `https://api.memegen.link/images/${memeTemplateName}/${topText}/${bottomText}.jpg`,
                 );
               }}
             >
@@ -128,6 +143,7 @@ function App() {
             </button>
           </div>
         </div>
+        {/* to say that the button will go on the right side */}
         <div id="right">
           <div>
             <img data-test-id="meme-image" src={url} alt="Custom meme" />
